9d78e01c6232c88fa8b3912494e282756b9a45c3
timothyrjames <timothyrjames@yahoo.com>
2019-02-03 22:02:59 -0500

    Updating new examples.

---
+++ project2_html/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return redirect("/static/index.html", code=302)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
