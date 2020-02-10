5bd169c5f9a5637bd040cf2ec362f15574efecfc
Timothy James <timothyrjames@gmail.com>
2020-02-10 14:41:42 -0500

    updated HTML formatting, fixed examples

--- week05/gae/project3/main.py
+++ week05/gae/project3/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return redirect("/static/index.html", code=302)
+    return redirect("/s/index.html", code=302)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
